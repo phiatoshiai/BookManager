9ed0c998e05ff851a835e593041da8fdbc0e7dff
phuphi <phi.atoshiai@gmail.com>
2019-06-02 01:57:07 +0700

    insert liquibase

--- src/main/resources/db/changelog/sql/create.sql
+++ src/main/resources/db/changelog/sql/create.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 8.0.15, for Win64 (x86_64)
 --
--- Host: 127.0.0.1    Database: book_manager
+-- Host: 127.0.0.1    Database: bookmanager
 -- ------------------------------------------------------
 -- Server version	8.0.15
 
